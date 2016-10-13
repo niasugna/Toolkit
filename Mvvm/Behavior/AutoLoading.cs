9d14034f2b2a718a245942307d47b9c89cccca4f
niasugna <angusain@hotmail.com>
2016-10-13 10:15:17 +0800

    auto loading

---
+++ Mvvm/Behavior/AutoLoading.cs
@@ -0,0 +1,43 @@
+﻿using Pollux.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Pollux.Behavior
+{
+    public class AutoLoading
+    {
+        static AutoLoading()
+        {
+        }
+
+
+
+        public static bool GetAutoLoading(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoLoadingProperty);
+        }
+
+        public static void SetAutoLoading(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoLoadingProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for AutoLoading.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AutoLoadingProperty =
+            DependencyProperty.RegisterAttached("AutoLoading", typeof(bool), typeof(AutoLoading), new PropertyMetadata(false,
+                (s, e) =>
+                {
+                    var vm = (s as FrameworkElement).DataContext as BusyViewModelBase;
+                    if (vm !=null)
+                    {
+                        vm.IsLoaded.Value.Refresh();
+                    }
+                }));
+
+        
+    }
+}
